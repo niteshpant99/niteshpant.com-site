5d76125dc1ca9fc047da078fe9e7e90260914c62
Nitesh Pant <133068221+niteshpant99@users.noreply.github.com>
2025-01-02 23:01:08 -0500

    Update config.ts

--- app/config.ts
+++ app/config.ts
@@ -1,16 +1,16 @@
 export const metaData = {
-  baseUrl: "https://nextfolio-template.vercel.app/",
-  title: "Nextfolio",
-  name: "Sirius",
+  baseUrl: "https://niteshpant.com/",
+  title: "Nitesh Pant Portfolio",
+  name: "Nitesh",
   ogImage: "/opengraph-image.png",
   description:
-    "A clean, fast, and lightweight portfolio template built with Next.js, Vercel, and Tailwind CSS for optimal performance.",
+    "Portfolio page of Nitesh Pant. Co-founder @ DevDash Labs. Former management consultant. Dartmouth College educated political economist.",
 };
 
 export const socialLinks = {
-  twitter: "https://x.com/1tssirius",
-  github: "https://github.com/1msirius/Nextfolio",
+  twitter: "https://x.com/niteshpant56",
+  github: "https://github.com/niteshpant99",
   instagram: "https://www.instagram.com/",
-  linkedin: "https://www.linkedin.com/",
-  email: "mailto:example@gmail.com",
+  linkedin: "https://www.linkedin.com/in/niteshpant22/",
+  email: "mailto:nitesh.pant@devdashlabs.com",
 };
