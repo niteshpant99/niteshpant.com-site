2dfae8be4110ceeef5f8ae84c7db6f7c16bf19d1
niteshpant99 <nitesh.2056@gmail.com>
2025-01-18 23:23:27 -0500

    changed blogs to essays

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 import { formatDate, getBlogPosts } from "app/lib/posts";
 
 export const metadata = {
-  title: "Blog",
-  description: "Nextfolio Blog",
+  title: "Essays",
+  description: "Essasy by Nitesh Pant",
 };
 
 export default function BlogPosts() {
@@ -12,7 +12,11 @@ export default function BlogPosts() {
 
   return (
     <section>
-      <h1 className="mb-8 text-2xl font-medium tracking-tight">Our Blog</h1>
+      
+      <h1 className="mb-2 text-2xl font-medium tracking-tight">Essays</h1>
+      <div>
+      <h2 className="mb-6 text-l">by Nitesh</h2>
+      </div>
       <div>
         {allBlogs
           .sort((a, b) => {
@@ -28,7 +32,7 @@ export default function BlogPosts() {
             <Link
               key={post.slug}
               className="flex flex-col space-y-1 mb-4 transition-opacity duration-200 hover:opacity-80"
-              href={`/blog/${post.slug}`}
+              href={`/essays/${post.slug}`}
             >
               <div className="w-full flex flex-col sm:flex-row justify-between items-start sm:items-center space-y-1 sm:space-y-0 sm:space-x-2">
                 <p className="text-black dark:text-white tracking-tight">
