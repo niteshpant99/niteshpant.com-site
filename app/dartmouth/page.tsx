ee92ee4a250464f8e2a37befcc214ff29fbbb489
niteshpant99 <nitesh.2056@gmail.com>
2025-01-24 22:38:56 -0500

    updated /dartmouth

--- app/dartmouth/page.tsx
+++ app/dartmouth/page.tsx
@@ -12,10 +12,10 @@ export default function Dartmouth() {
         <section>
             <h1 className="mb-8 text-2xl font-medium tracking-tight">Dartmouth</h1>
             
-            <p>Many times, be it when I'm interviewing or talking to new folks, I get asked one question - how did you make it to Dartmouth?</p>
-            <p>How did a kid born in Dhangadhi, Nepal go on to study at Dartmouth College in Hanover, NH?</p>
-            <p>The answer lies in a lot of hard work, thoughtful planning, and luck. Not to mention immense support from my parents.</p>
-            <p>But the journey was not easy. It was filled with ups and downs, moments of joy and moments of despair. (Cursour Tab wrote this??)</p>
+            <p className="mb-2">Many times, be it when I'm interviewing or talking to new folks, I get asked one question - how did you make it to Dartmouth?</p>
+            <p className="mb-2">How did a kid born in Dhangadhi, Nepal go on to study at Dartmouth College in Hanover, NH?</p>
+            <p className="mb-2">The answer lies in a lot of hard work, thoughtful planning, and luck. Not to mention immense support from my parents.</p>
+            <p className="mb-2">But the journey was not easy. It was filled with ups and downs, moments of joy and moments of despair. (Cursour Tab wrote this!?)</p>
         </section>
     )
 }
