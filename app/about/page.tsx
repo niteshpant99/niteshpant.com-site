fd42e664ef38ec03807c51ac73235a36e5f6121a
niteshpant99 <nitesh.2056@gmail.com>
2025-01-28 22:01:23 -0500

    fixed about page with meteors

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { metaData } from "../config";
 import { Metadata } from "next";
+import { Meteors } from "../components/ui/meteors";
+
 
 export const metadata: Metadata = {
     title: "About",
@@ -9,8 +11,14 @@ export const metadata: Metadata = {
 
 export default function About() {
   return (
+    
     <div>
-      <h1 className="text-xl mb-8">About Nitesh</h1>
+      <div className="relative flex h-[100px] w-full flex-col items-center justify-center overflow-hidden rounded-lg bg-background ">
+      <Meteors number={30} />
+      <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10">
+        <h1>About</h1>
+      </span>
+    </div>
 
       <p className="mb-4">
         I am an economist by training, consultant by trade, and a technology enthusiast at heart. Born and raised in Nepal, I came to the United States in 2018 to study at Dartmouth College, where I majored in Economics. My journey from the remote Far-Western region of Nepal to the Ivy League shaped my perspective on the transformative power of opportunity and education. After graduating in 2023, I worked as a management consultant at Roland Berger, focusing on healthcare and chemical industry projects.
