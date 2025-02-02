bf0ffe66ad4c91db48089edbc22320f311c65b8a
niteshpant99 <nitesh.2056@gmail.com>
2025-02-02 17:29:02 -0500

    updated work

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -13,8 +13,11 @@ export default function Work() {
       <h1 className="mb-2 text-2xl font-medium tracking-tight">Work</h1>
       <p>A selection of work I've done</p>
       <h2 className="mb-4 pl-4 pt-4 text-xl">alkemy platform for Roland Berger</h2>
+      <p className="mb-2">alkemy is a platform that allows Roland Berger to model any chemical asset in the world based on the technology used and raw material inputs</p>
       <h2 className="mb-4 pl-4 pt-4 text-xl">Hercules Transformation</h2>
+      <p className="mb-2">Created a GTM framework for Hercules, a healthtech SaaS. From an updated brand identity, to ICP, price modeling, to crafting an online presence - did it all.</p>
       <h2 className="mb-4 pl-4 pt-4 text-xl">DevDash Metamorphosis</h2>
+      <p className="mb-2">Strategic pivot of DevDash from a general IT consulting company to an AI research and development company</p>
       <h2 className="mb-4 pl-4 pt-4 text-xl">Novodaya</h2>
       <h2 className="mb-4 pl-4 pt-4 text-xl">Luna</h2>
       <h2 className="mb-4 pl-4 pt-4 text-xl">Safka</h2>
